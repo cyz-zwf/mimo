cd4a6ad2147369b183567a8e2e579fc7b7f7580c
cyz <cyz@tedu.cn>
2019-08-12 20:34:54 +0800

    Home

---
+++ mimo-vue/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <mt-tab-container v-model="active">
+            <mt-tab-container-item id="Find">
+                发现内容
+            </mt-tab-container-item >
+            <mt-tab-container-item id="Collection">
+                收藏
+            </mt-tab-container-item >
+            <mt-tab-container-item id="Message">
+                消息
+            </mt-tab-container-item>
+            <mt-tab-container-item id="Mine">
+                我
+            </mt-tab-container-item>
+        </mt-tab-container>
+        <!-- 底部导航条 -->
+        <mt-tabbar v-model="active" fixed>
+            <mt-tab-item id="Find">
+                发现内容
+            </mt-tab-item>
+            <mt-tab-item id="Collection">
+                收藏
+            </mt-tab-item>
+            <mt-tab-item id="Message">
+                消息
+            </mt-tab-item>
+            <mt-tab-item id="Mine">
+                我
+            </mt-tab-item>
+        </mt-tabbar>
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            active:""
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
--- mimo-vue/src/main.js
+++ mimo-vue/src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-import MintUi from 'munt-ui'
+import MintUi from 'mint-ui'
 import "mint-ui/lib/style.css"
 
 
-Vue.use(MintUi);
-Vue.prototype.axios=axios
-axios.defaults.withCredentials=true
 
+Vue.prototype.axios = axios
+axios.defaults.withCredentials = true
+Vue.use(MintUi);
 Vue.config.productionTip = false
 
 new Vue({
