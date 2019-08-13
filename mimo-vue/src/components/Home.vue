6eb3b84f242b2c9bc1d2e8c5a202f0182c74626c
cyz <cyz@tedu.cn>
2019-08-13 20:44:10 +0800

    home

--- mimo-vue/src/components/Home.vue
+++ mimo-vue/src/components/Home.vue
@@ -16,16 +16,36 @@
         </mt-tab-container>
         <!-- 底部导航条 -->
         <mt-tabbar v-model="active" fixed>
-            <mt-tab-item id="Find">
+            <mt-tab-item id="Find" @click.native="change(0)">
+                <tabbaricon 
+                :selected="require('../assets/ic_balloon_selected.png')"
+                :normal="require('../assets/ic_balloon_normal.png')"
+                :focused="currentIndex[0].isSelect"
+                ></tabbaricon>
                 发现内容
             </mt-tab-item>
-            <mt-tab-item id="Collection">
+            <mt-tab-item id="Collection"  @click.native="change(1)">
+                <tabbaricon 
+                :selected="require('../assets/ic_heart_selected.png')"
+                :normal="require('../assets/ic_heart_normal.png')"
+                :focused="currentIndex[1].isSelect"
+                ></tabbaricon>
                 收藏
             </mt-tab-item>
-            <mt-tab-item id="Message">
+            <mt-tab-item id="Message"  @click.native="change(2)">
+                <tabbaricon 
+                :selected="require('../assets/ic_message_selected.png')"
+                :normal="require('../assets/ic_message_normal.png')"
+                :focused="currentIndex[2].isSelect"
+                ></tabbaricon>
                 消息
             </mt-tab-item>
-            <mt-tab-item id="Mine">
+            <mt-tab-item id="Mine"  @click.native="change(3)">
+                <tabbaricon 
+                :selected="require('../assets/ic_cat_selected.png')"
+                :normal="require('../assets/ic_cat_normal.png')"
+                :focused="currentIndex[3].isSelect"
+                ></tabbaricon>
                 我
             </mt-tab-item>
         </mt-tabbar>
@@ -33,14 +53,45 @@
     </div>
 </template>
 <script>
+import Tabbaricon from './Tabbaricon.vue'
 export default {
+    components:{
+        tabbaricon:Tabbaricon
+    },
     data(){
         return {
-            active:""
+            active:"Find",
+            currentIndex:[
+                {isSelect:true},
+                {isSelect:false},
+                {isSelect:false},
+                {isSelect:false},
+            ]
+        }
+    },
+    methods:{
+        change(idx){
+            for(var i=0;i<this.currentIndex.length;i++){
+                var ui=idx;
+                if(ui == i ){
+                    this.currentIndex[i].isSelect=true;
+                }else{
+                    this.currentIndex[i].isSelect=false;
+                }
+            }
         }
     }
+    
 }
 </script>
 <style scoped>
-    
+/* 底部导航条组件默认样式  */
+.mint-tabbar > .mint-tab-item {
+  color: #999;
+}
+/* 修改组件选择的样式 */
+.mint-tabbar > .mint-tab-item.is-selected {
+  background-color: transparent;
+  color: #ff1e32;
+}
 </style>
