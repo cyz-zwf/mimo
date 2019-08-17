15198637f4a398292d34261138d3096db63a43d2
cyz <cyz@tedu.cn>
2019-08-17 17:33:28 +0800

    Me.vue

--- mimo-vue/src/components/Me/Me.vue
+++ mimo-vue/src/components/Me/Me.vue
@@ -19,41 +19,56 @@
     </div>
     <!-- 我的订单 -->
     <div class="bg_box">
-      <!-- <melist></melist>
-      <melist></melist>-->
-      <div class="order" v-for="(item,i ) of list" :key="i">
-        <div v-cloak>{{item.title}}</div>
-          <ul>
-            <li>
-              <img :src="`http://127.0.0.1:5050/Me/${item.one.pic}`" />
-              <p v-cloak>{{item.one.title}}</p>
-              <p class="bg_list" v-cloak>{{item.one.desc}}</p>
-            </li>
-            <li>
-              <img :src="`http://127.0.0.1:5050/Me/${item.two.pic}`" />
-              <p v-cloak>{{item.two.title}}</p>
-              <p class="bg_list" v-cloak>{{item.two.desc}}</p>
-            </li>
-            <li>
-              <img :src="`http://127.0.0.1:5050/Me/${item.three.pic}`" />
-              <p v-cloak>{{item.three.title}}</p>
-              <p class="bg_list" v-cloak>{{item.three.desc}}</p>
-            </li>
-          </ul>
-
+      <div class="order">
+      <div>我的订单</div>
+      <div>
+        <ul>
+          <li>
+            <img src="http://127.0.0.1:5050/Me/ic_dd.png" />
+            <p>全部订单</p>
+            <p class="bg_list">查看订单详情</p>
+          </li>
+          <li>
+            <img src="http://127.0.0.1:5050/Me/ic_fp.png" />
+            <p>开具发票</p>
+            <p class="bg_list">发票历史及抬头</p>
+          </li>
+          <li>
+            <img src="http://127.0.0.1:5050/Me/ic_ms.png" />
+            <p>打开门锁</p>
+            <p class="bg_list">预定后可使用</p>
+          </li>
+        </ul>
       </div>
     </div>
-    <!-- 我的钱包 -->
-    <!-- <div class="momy">
-          <ul>
-            <li>
-              <img src="http://127.0.0.1:5050/Me/ic_dd.png" />
-              <p>全部订单</p>
-              <p class="bg_list">查看订单详情</p>
-            </li>
-          </ul>
-        </div>
-    </div>-->
+      <mt-swipe :auto="4000" class="swipe">
+          <mt-swipe-item><img src="http://127.0.0.1:5050/inhome/seimg/2f8.jpg" style="width:100%"></mt-swipe-item>
+          <mt-swipe-item><img src="http://127.0.0.1:5050/inhome/seimg/4c.jpg" style="width:100%"></mt-swipe-item>
+          <mt-swipe-item><img src="http://127.0.0.1:5050/inhome/seimg/ca38.jpg" style="width:100%"></mt-swipe-item>
+      </mt-swipe>
+      <div class="order" v-for="(item,i ) of list" :key="i">
+        <div v-cloak>{{item.title}}</div>
+        <ul>
+          <li>
+            <img :src="`http://127.0.0.1:5050/Me/${item.one.pic}`" />
+            <p v-cloak>{{item.one.title}}</p>
+            <p class="bg_list" v-cloak>{{item.one.desc}}</p>
+          </li>
+          <li>
+            <img :src="`http://127.0.0.1:5050/Me/${item.two.pic}`" />
+            <p v-cloak>{{item.two.title}}</p>
+            <p class="bg_list" v-cloak>{{item.two.desc}}</p>
+          </li>
+          <li>
+            <img :src="`http://127.0.0.1:5050/Me/${item.three.pic}`" />
+            <p v-cloak>{{item.three.title}}</p>
+            <p class="bg_list" v-cloak>{{item.three.desc}}</p>
+          </li>
+        </ul>
+      </div>
+      <!-- 我要成为房东 -->
+      <div class="change">我要成为房东</div>
+    </div>
   </div>
 </template>
 <script>
@@ -61,22 +76,49 @@ import Melist from "./Melist";
 export default {
   data() {
     return {
-      list:[
-        {title:"我的订单",one:{pic:"ic_dd.png",title:"全部订单",desc:"查看全部订单"},two:{pic:"ic_fp.png",title:"开具发票",desc:"发票历史及抬头"},three:{pic:"ic_ms.png",title:"打开门锁",desc:"预定后可使用"}},
-        {title:"我的钱包",one:{pic:"ic_fx.png",title:"邀请好友",desc:"代金券多邀多得"},two:{pic:"ic_djj.png",title:"代金券",desc:"¥ 0"},three:{pic:"ic_jf.png",title:"积分",desc:"0"}},
-        {title:"我的特权",one:{pic:"ic_mian.png",title:"免押入住",desc:"未开通"},two:{pic:"slrz.png",title:"商旅认证",desc:"未认证"},three:{pic:"ic_dianbo.png",title:"点评",desc:"入住后可发表"}},
-        {title:"更多功能",one:{pic:"ic_dw.png",title:"收件地址",desc:"管理收件地址"},two:{pic:"ic_ren.png",title:"常用联系人",desc:"共0人入住"},three:{pic:"ic_call.png",title:"联系客服",desc:"在线解答疑问"}},
-        ]
-    }
+      list: [
+        // {
+        //   title: "我的订单",
+        //   one: { pic: "ic_dd.png", title: "全部订单", desc: "查看全部订单" },
+        //   two: { pic: "ic_fp.png", title: "开具发票", desc: "发票历史及抬头" },
+        //   three: { pic: "ic_ms.png", title: "打开门锁", desc: "预定后可使用" }
+        // },
+        {
+          title: "我的钱包",
+          one: { pic: "ic_fx.png", title: "邀请好友", desc: "代金券多邀多得" },
+          two: { pic: "ic_djj.png", title: "代金券", desc: "¥ 0" },
+          three: { pic: "ic_jf.png", title: "积分", desc: "0" }
+        },
+        {
+          title: "我的特权",
+          one: { pic: "ic_mian.png", title: "免押入住", desc: "未开通" },
+          two: { pic: "slrz.png", title: "商旅认证", desc: "未认证" },
+          three: { pic: "ic_dianbo.png", title: "点评", desc: "入住后可发表" }
+        },
+        {
+          title: "更多功能",
+          one: { pic: "ic_dw.png", title: "收件地址", desc: "管理收件地址" },
+          two: { pic: "ic_ren.png", title: "常用联系人", desc: "共0人入住" },
+          three: { pic: "ic_call.png", title: "联系客服", desc: "在线解答疑问" }
+        }
+      ],
+      car:false,
+    };
+  },
+  methods:{
+    
   },
   components: {
     melist: Melist
+  },
+  mounted() {
+    
   }
 };
 </script>
 <style scoped>
-[v-cloak]{
-  display:none !important;
+[v-cloak] {
+  display: none !important;
 }
 .top {
   height: 8rem;
@@ -86,7 +128,7 @@ export default {
   color: #fff;
   background: linear-gradient(to top, #f16b80 0%, rgb(230, 171, 171) 100%);
 }
-.top::before{
+.top::before {
   content: "";
   display: table;
 }
@@ -103,7 +145,7 @@ export default {
   height: 3rem;
   position: absolute;
   top: 25%;
-  left:5%;
+  left: 5%;
   width: 90%;
 }
 .lt {
@@ -170,32 +212,33 @@ export default {
 .bg_box {
   padding: 0 0.5rem;
   background: #e8e8e8;
+  padding-bottom: 0.6rem;
 }
- .order {
+.order {
   font-size: 0.4rem;
   height: 4rem;
   padding: 0.3rem;
   background: #fff;
   border-bottom-left-radius: 0.3rem;
   border-bottom-right-radius: 0.3rem;
-   margin-bottom:0.4rem;
+  margin-bottom: 0.4rem;
 }
 .order ul {
-  padding:0;
+  padding: 0;
   list-style-type: none;
   display: flex;
   justify-content: space-around;
-  margin-top:0.7rem;
+  margin-top: 0.7rem;
 }
-.order ul li{
-  width:30%;
-  display:flex;
+.order ul li {
+  width: 30%;
+  display: flex;
   flex-direction: column;
-  align-items: center
+  align-items: center;
 }
-.order ul li img{
-  width:1rem;
-  height:1rem;
+.order ul li img {
+  width: 1rem;
+  height: 1rem;
 }
 .order ul li p {
   margin-top: 0.1rem;
@@ -204,9 +247,19 @@ export default {
   color: #999;
   font-size: 0.15rem;
   margin-top: -0.2rem;
-} 
-/* 我的钱包 */
-.momy {
-  height: 2rem;
+}
+/* 轮播图 */
+.swipe{
+  height:5rem;
+  margin-bottom:0.4rem;
+}
+.change{
+  height:1.3rem;
+  background:#fff;
+  margin-bottom:1.3rem;
+  color:#666;
+  font-size:0.5rem;
+  text-align:center;
+  line-height:1.3rem;
 }
 </style>
