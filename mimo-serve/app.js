3f3d9d4e7f2e9cfed22b88ae5ee2c60e5df54f37
ZhengYuanFa <303252394@qq.com>
2019-08-23 09:12:10 +0800

    全球房源

--- mimo-serve/app.js
+++ mimo-serve/app.js
@@ -29,3 +29,30 @@ app.use(session({
 //托管
 app.use(express.static("public"));
 app.listen(5050);
+
+
+//主页全球热门房源
+app.get("/global",(req,res)=>{
+    var sql = "SELECT img_url,title,subtitle,price,comment from global_house";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send({
+            code:1,
+            msg:"查询成功",
+            data:result
+        })
+    });
+})
+
+//高分体验
+app.get("/highScore",(req,res)=>{
+    var sql = "SELECT img_url,title,subtitle,price,comment from high_score";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send({
+            code:1,
+            msg:"查询成功",
+            data:result
+        })
+    });
+})
--- mimo-vue/src/router.js
+++ mimo-vue/src/router.js
@@ -4,12 +4,12 @@ import Router from 'vue-router'
 import Home from './components/Home.vue' //主页面
 import Detail from './components/Details/Detail.vue' //主页面
 import HeaderSe from './components/Index/HeaderSe.vue' // 主页轮播图
+import GlobalHouse from "./components/Index/GlobalHouse.vue"
 
 Vue.use(Router)
 
 export default new Router({
    routes: [
-
       {
          path: '/home',
          component: Home
@@ -25,6 +25,10 @@ export default new Router({
       {
          path: '/headerse',
          component: HeaderSe
+      },
+      {
+         path:'/GlobalHouse',
+         component:GlobalHouse
       }
    ]
 })
